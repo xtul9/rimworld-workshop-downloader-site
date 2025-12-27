bc0af34bed4ea21a44bf931ef4cd6d0618529395
Michał Piasecki <mpiasecki720@protonmail.com>
2025-12-27 23:24:24 +0100

    add app preview

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,16 @@
+import logo from '../assets/rwd.png'
+
 function Header() {
   return (
     <header className="header">
       <div className="container">
-        <h1 className="header-title">Rimworld Workshop Downloader</h1>
-        <p className="header-subtitle">Download and manage Rimworld mods from Steam Workshop</p>
+        <div className="header-content">
+          <img src={logo} alt="Rimworld Workshop Downloader" className="header-image" />
+          <div className="header-text">
+            <h1 className="header-title">Rimworld Workshop Downloader</h1>
+            <p className="header-subtitle">Download and manage Rimworld mods from Steam Workshop</p>
+          </div>
+        </div>
       </div>
     </header>
   )
